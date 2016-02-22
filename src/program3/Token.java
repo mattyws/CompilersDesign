10af1b080a38ca6aab09b84da8f25187724217aa
VitorFinati <vitorfinati@gmail.com>
2016-02-21 19:45:18 -0500

    Adicionei o metodo next na classe Token

--- src/program3/Token.java
+++ src/program3/Token.java
@@ -4,9 +4,9 @@ public abstract class Token {
 	
 	public String text;
 	public int pos;
-	public boolean valid;
+	//public boolean valid;
 
 	public abstract void getToken();
-	public abstract void isValid(boolean value);
+	//public abstract void isValid(boolean value);
 	public abstract void next();
 }
