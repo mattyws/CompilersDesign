1fb9166a495f2a319702498fdea30706fe060ccc
VitorFinati <vitorfinati@gmail.com>
2016-02-21 21:18:53 -0500

    Classes criadas.

---
+++ src/program3/TokenLBrace.java
@@ -0,0 +1,37 @@
+package program3;
+
+import java.util.List;
+
+public class TokenLBrace extends Token {	
+	
+	public TokenLBrace(String program, int pos) {
+		this.program = program;
+		this.pos = pos;
+		this.value = "{";
+	}
+
+	@Override
+	public void getToken() {
+		this.setValid(true);
+	}
+
+	@Override
+	public Token next() {
+		Token t = null;
+		if(program.charAt(pos)=='i'){
+			
+		}
+		return t;
+		
+	}
+
+	@Override
+	public boolean isValid() {
+		return this.valid;
+	}
+
+	@Override
+	public void setValid(boolean value) {
+		this.valid = value;
+	}
+}
